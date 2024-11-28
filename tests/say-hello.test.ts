b8e67b27e2a2a2b3454565822dd10f586a3597aa
kieranwv <kieranwme@gmail.com>
2024-11-28 10:32:37 +0800

    chore: improve description for sayHello test case

--- tests/say-hello.test.ts
+++ tests/say-hello.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import { sayHello } from '../src/say-hello'
 
-describe('test use for say-hello.ts', () => {
-  it('base', () => {
+describe('test for say-hello.ts', () => {
+  it('should return a greeting message', () => {
     const res = sayHello('Kieran')
     expect(res).toBe('Hello, Kieran!')
   })
